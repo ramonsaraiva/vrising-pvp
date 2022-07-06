0d8d3383c22de45d8f4aa39592d04c990fd14ee9
Ramon Saraiva <ramonsaraiva@gmail.com>
2022-07-06 00:55:43 -0300

    Fix deploy

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
 @echo off
 dotnet build
-copy /y "bin\Debug\netstandard2.1\VRising.TestPlugin2.dll" "C:\Program Files (x86)\Steam\steamapps\common\VRisingDedicatedServer\BepInEx\WetstonePlugins\"
+copy /y "bin\Debug\netstandard2.1\VRising.PVP.dll" "C:\Program Files (x86)\Steam\steamapps\common\VRisingDedicatedServer\BepInEx\WetstonePlugins\"
